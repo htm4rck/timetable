654e95e288dd35989daebff837d7bc19ca1ec92f
Marc Vilchez <ht.m4rck@gmail.com>
2019-06-29 21:51:16 -0500

    Headers

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
-error_reporting(E_ALL);
+/*error_reporting(E_ALL);
 ini_set('display_errors', '1');
+*/
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Credentials: true");
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+//header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
 
 #Enrutador y Utilitarios
 require_once "config/BeanResource.php";
